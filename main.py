8d49562b6d6b0bf127b13a685c40a7551cabc15d
Овчинников Кирилл Дмитриевич <Kirill.Ovchinnikov@norbit.ru>
2022-08-15 19:00:44 +0300

    last today

--- main.py
+++ main.py
@@ -1,3 +1,6 @@
+import math
+
+
 class Dish:
   def __init__(self, parameters):
     self.name = parameters[0]
@@ -13,8 +16,8 @@ class Ingridient:
 class Prices:
   def __init__(self, parameters):
     self.name = parameters[0]
-    self.price = parameters[1]
-    self.count = parameters[2]
+    self.price = int(parameters[1])
+    self.count = int(parameters[2])
     self.izmer = parameters[3]
 
 class Bju:
@@ -34,8 +37,9 @@ def get_dishes(count_dishes, dishes, ingridients):
     for j in range(0, int(dish[2])):
       ingr = input().split(" ")
       if ingr[0] in ingridients:
-        ingridients[ingr[0]] += int()
-      
+        ingridients[ingr[0]] += int(ingr[1]) * int(dish[1])
+      else:
+        ingridients[ingr[0]] = int(ingr[1]) * int(dish[1])
       ingridient = Ingridient([ingr[0], ingr[1], ingr[2]])
       dish_ingridients.append(ingridient)
     dishes.append(Dish([dish[0], dish[1], dish_ingridients]))
@@ -52,24 +56,43 @@ def get_bju(count_ingridients, bju):
 
 def main():
   dishes = []
-  ingridients = []
+  ingridients = {}
+  products = {}
   prices = []
   bju = []
+  dict_izmer = {
+    'tens': 10,
+    'l': 1000,
+  }
   get_dishes(input(), dishes, ingridients)
   get_prices(input(), prices)
   get_bju(input(), bju)
+  all_price = 0
+  for i in range(0, len(prices)):
+    if prices[i].name in ingridients:
+      if prices[i].izmer in dict_izmer:
+        products[prices[i].name] = math.ceil(ingridients[prices[i].name] / dict_izmer[prices[i].izmer])
+        all_price += (products[prices[i].name] * prices[i].price)
+      else:
+        products[prices[i].name] = math.ceil(ingridients[prices[i].name] / prices[i].count)
+        all_price += products[prices[i].name] * prices[i].price
+    else:
+      products[prices[i].name] = 0
 
-  # output_dishes()
-  # output_prices()
+  print(all_price)
+  print(products)
 
-def output_dishes():
+  # output_dishes(dishes)
+  # output_prices(prices)
+
+def output_dishes(dishes):
   print("\n----------DISHES RESULT-----------\n")
   for a in range(0, len(dishes)):
     print(dishes[a].name + " " + dishes[a].count_need)
     for b in range(0, len(dishes[a].ingridients)):
       print(dishes[a].ingridients[b].name + " " + dishes[a].ingridients[b].massa + " " + dishes[a].ingridients[b].izmer)
 
-def output_prices():
+def output_prices(prices):
   print("\n----------PRICES RESULT-----------\n")
   for a in range(0, len(prices)):
     print(prices[a].name + " " + prices[a].price + " " + prices[a].count + " " + prices[a].izmer)
